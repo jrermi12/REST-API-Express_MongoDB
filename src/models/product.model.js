0e1e53841c891bd117e58363c32bf920bcd56772
Ermias Asmare <61870061+jrermi12@users.noreply.github.com>
2023-05-05 09:24:41 +0300

    created the product model

---
+++ src/models/product.model.js
@@ -0,0 +1,21 @@
+//Importing Libraries
+const mongoose = require("mongoose");
+
+//using monoose to create the product schema
+const productSchema = mongoose.Schema({
+    productId:{
+        type:String
+    },
+    name:{
+        type:String, 
+    },
+    description:{
+        type:String,
+    },
+    price:{
+        type:Number, 
+    },
+ });
+
+//exporting the product schema as monngose collection
+module.exports = mongoose.model('Product', productSchema);
