022f5eb5792d534c796643f95e861348cad1e6f5
Ermias Asmare <61870061+jrermi12@users.noreply.github.com>
2023-05-05 09:26:37 +0300

    imported the product routes to index

--- index.js
+++ index.js
@@ -13,12 +13,16 @@ const connectToDB = require("./src/config/db_config");
 //calling the function or running the function
 connectToDB();
 
+//Importing the product routes module
+const product = require("./src/routes/product.routes")
 
 //Adding Node features
 app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({ limit:"50mb", extended: true}));
 app.use(cors());
 
+//using the product route 
+app.use("/api/product", product)
 
 //Run Node APP
 module.exports = app
