13dac9c1202f1e0741d2c46f9d98ceb6e24875cc
Ermias Asmare <61870061+jrermi12@users.noreply.github.com>
2023-05-10 10:29:28 +0300

    created the test data for product test

---
+++ src/utils/data/product.test.data.js
@@ -0,0 +1,16 @@
+const reqaddProduct = {
+    name:"Product test", 
+    description:"This is product test", 
+    price:200
+ }
+
+const requpdateProduct = {
+    name:"Product update", 
+    description:"This is product test for update", 
+    price:100
+ }
+
+module.exports = {
+  reqaddProduct, 
+  requpdateProduct
+}
